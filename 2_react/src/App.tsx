b25b2af6b48b18e6e79e969b88c604f23ff26a37
Markus Karlsen <markus.karlsen@bekk.no>
2018-10-28 10:07:08 +0100

    Oppdatert 2_react oppgave

--- 2_react/src/App.tsx
+++ 2_react/src/App.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
 import './App.css';
-import NameForm from './fasit/_NameForm';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <NameForm label="Navn" />
       </div>
     );
   }
