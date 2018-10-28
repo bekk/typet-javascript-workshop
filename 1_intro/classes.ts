6953f2873e3c522aebdfc8599eb01c02ea3366df
Tor Egil Fusdahl <te.fusdahl@gmail.com>
2018-10-28 09:29:13 +0100

    Create classes.ts

---
+++ 1_intro/classes.ts
@@ -0,0 +1,2 @@
+// Kort gjennomgang av klasser. Fungerer i veldig stor grad slik vi er vant til fra java c# etc
+// Lag et eksempel hvor vi demonstrerer konstruktør, private, public, readonly og arv.
