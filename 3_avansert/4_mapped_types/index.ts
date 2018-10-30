99c0a9139854b2a8f65bba7076661b0f2cf87411
Erik Vesteraas <erik@vestera.as>
2018-10-30 16:22:04 +0100

    Add task to mapped types

--- 3_avansert/4_mapped_types/index.ts
+++ 3_avansert/4_mapped_types/index.ts
@@ -6,7 +6,12 @@
 
 type ValidationMessages<T extends {}> = { [K in keyof T]: string | undefined }
 
+// Vi kan da definere typen Validator<T> som en funksjons-type som returnerer et
+// objekt med feiltekst eller undefined for hver property i T:
 type Validator<T> = (data: T) => ValidationMessages<T>
+
+
+
 
 
 type FetchState<T>
@@ -16,3 +21,7 @@ type FetchState<T>
   | { status: 'FAILURE', error: unknown }
 
 type ResourceState<T extends {}> = { [K in keyof T]: FetchState<T[K]> };
+
+
+// Oppgave: Definer en type Partial<T> som gjør alle properties på T til
+// potensielt undefined
